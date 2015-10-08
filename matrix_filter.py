5c54accd90cfe0f8074cb5f309e695ab39a666b5
Alice <fieryrobin@gmail.com>
2015-10-08 13:26:39 +0200

    change names according to style

--- matrix_filter.py
+++ matrix_filter.py
@@ -1,7 +1,7 @@
 import numpy as np
 from PIL import Image, ImageDraw
 
-def getKernelDiv(kernel):
+def get_kernel_div(kernel):
 	div = np.sum(kernel)
 	if div <= 0 : div = 1
 	return div
@@ -11,24 +11,24 @@ def bound(x):
 	elif x > 255 : x = 255
 	return int(x)
 
-def boundPixel(pix):
+def bound_pixel(pix):
 	return (bound(pix[0]), bound(pix[1]), bound(pix[2]))
 
-def bordersCorrect(size, t, b, l, r):
+def borders_correct(size, t, b, l, r):
 	if t < 0 or l < 0 or (b > size[0]) or (r > size[1]):
 		return False
 	else: return True
 
-def doConvolution(section, kernel, size):
+def convolve(section, kernel, size):
 	r, g, b = 0, 0, 0
-	div = getKernelDiv(kernel)
+	div = get_kernel_div(kernel)
 
 	for i in range(size[0]):
 		for j in range(size[1]):
 			r += (section[i,j][0] * kernel[i,j])//div 
 			g += (section[i,j][1] * kernel[i,j])//div 
 			b += (section[i,j][2] * kernel[i,j])//div 
-	return boundPixel((r,g,b))
+	return bound_pixel((r,g,b))
 
 def filter(original, result, kernel, size):
 	kSize = kernel.shape
@@ -38,15 +38,15 @@ def filter(original, result, kernel, size):
 			bottom = row + kSize[0]//2 + 1
 			left = col - kSize[1]//2
 			right = col + kSize[1]//2 + 1
-			if not bordersCorrect(size, top, bottom, left, right): continue
+			if not borders_correct(size, top, bottom, left, right): continue
 
 			section = original[top:bottom, left:right]
-			rgb = doConvolution(section, kernel, kSize)
+			rgb = convolve(section, kernel, kSize)
 			result.point((col, row), (rgb[0], rgb[1], rgb[2]))
 
 def process(image, kernel):
 	width = image.size[0]
 	height = image.size[1]
-	originalArr = np.array(image)
+	original = np.array(image)
 	draw = ImageDraw.Draw(image)
-	filter(originalArr, draw, kernel, (height, width))
+	filter(original, draw, kernel, (height, width))
